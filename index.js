96f297850d335731d7c20eea103141a2b5733a79
mo-cherif <mocherifh0@gmail.com>
2023-04-03 11:37:52 +0000

    🤞

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const {ApolloServer, gql} = require("apollo-server");
 
+
+// our type definitions
 const typeDefs = gql`
     type Query {
         totalDays: Int!
     }
 `;
 
+// creating appolo server here
 const server = new ApolloServer ({
     typeDefs,
     mocks: true
 });
 
-
+// listen 
 server.listen().then(({url}) => {
     console.log(`server is running ${url}`);
 });
