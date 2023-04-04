7310d7add85dc4377667f274bc125b987703e4bc
mo-cherif <mocherifh0@gmail.com>
2023-04-04 20:09:37 +0000

    x

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const {ApolloServer, gql} = require("apollo-server");
 // our type definitions
 const typeDefs = gql`
 
+    scalar Date
+
     type SkiDay {
         id: ID!
-        date: String!
+        date: Date!
         moutain: String!
         conditions: Conditions
     }
@@ -23,9 +25,15 @@ const typeDefs = gql`
         allDays: [SkiDay!]!
     }
 
+    input AddDayInput {
+        date: Date!
+        mountain: String!
+        conditions: Conditions
+    }
     type Mutation {
+        addDay(input: AddDayInput!): SkiDay
         removeDay(id: ID!): SkiDay!
-        
+
     }
 `;
 
